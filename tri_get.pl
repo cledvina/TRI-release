cc4295691af078b6d82f7050b01af03854c2ef19
Charles Ledvina <charles@indexdata.com>
2017-04-22 14:07:31 -0500

    added csv support

--- tri_get.pl
+++ tri_get.pl
@@ -1,13 +1,18 @@
 #!/usr/bin/env perl
 use LWP::Simple;
+my $ext = shift;
+if ($ext !~ /^(html|csv)$/) {
+	print "Please enter extension (html or csv)\n";
+	exit;
+}
 open LINKS, 'paths.txt' or die "Can't find paths.txt file!\n";
-my $ext = 'html';
 my $data = '../data';
 my $i = 0;
 while (<LINKS>) {
 	chomp;
 	$i++;
 	my ($url, $fn) = split /\t/;
+	$url .= '&pOUTPUT=CSV' if $ext eq 'csv';
 	my $chem = $_;
 	my $year = $_;
 	$chem =~ s/.+&chemical=(\w+).*/$1/;
